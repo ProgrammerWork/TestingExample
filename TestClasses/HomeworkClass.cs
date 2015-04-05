9195dd0a3c9825e1b3bf3dbce9e02cb8a5c37fd0
Vladimir Malov <vldmalov@gmail.com>
2015-04-05 16:20:30 +0300

    homework6 mega refactoring HomeworkClass.DoSomething method

--- TestClasses/HomeworkClass.cs
+++ TestClasses/HomeworkClass.cs
@@ -1,42 +1,39 @@
 ﻿namespace TestClasses
 {
+    static public class StringUtils
+    {
+        static public void SwapStrings(ref string str1, ref string str2)
+        {
+            string buffStr = str1;
+            str1 = str2;
+            str2 = buffStr;
+        }
+    }
+
     public class HomeworkClass
     {
         public string[] DoSomething(string[] strings, string prefix = "", string postfix = "")
         {
-            if (strings == null) {
-                return null;
-            }
-
+            if (strings == null) return null;
             if (prefix == null) prefix = string.Empty;
             if (postfix == null) postfix = string.Empty;
 
-            // get array with input array length
+            if (prefix.Length + postfix.Length < 2)
+            {
+                StringUtils.SwapStrings(ref prefix, ref postfix);
+            }
+
             var s = new string[2 * strings.Length];
-            var effectivePrefix = prefix;
-            var effectivePostfix = postfix;
-            //if length of pre and postfixes are like this
-            var sumLength = prefix.Length + postfix.Length;
-            if (sumLength < 2)
+            for (var i = 0; i < strings.Length; ++i)
             {
-                effectivePrefix = postfix;
-                effectivePostfix = prefix;
-            }
-
-            for (var i = 0; i < strings.Length; i++)
-            {
-                s[i] = "MySuffix" + effectivePrefix + strings[i].ToLower();
-                s[strings.Length + i] = strings[i].ToLower() + "MySuffix" + effectivePostfix;
-            }
-
-            for (var i = strings.Length; i < s.Length; i++)
-            {
-                if (s[i].EndsWith("MySuffix") && effectivePostfix != "MySuffix")
-                {
-                    s[i - strings.Length] = s[i - strings.Length].Remove(0, "MySuffix".Length);
-                }
+                var current_string = strings[i].ToLower();
+                s[i] = (postfix.Length == 0) ? string.Empty : MAGIC_WORD;
+                s[i] += prefix + current_string;
+                s[strings.Length + i] = current_string + MAGIC_WORD + postfix;
             }
             return s;
         }
+
+        private const string MAGIC_WORD = "MySuffix";
     }
 }
